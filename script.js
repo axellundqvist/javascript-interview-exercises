75eaa6afdccdea6c08b2d5e1aae274606242b15e
axellundqvist <axellundkvistrhotmail.com>
2019-04-15 21:24:59 +0200

    first exercise, first commit

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-(() => {
+const companies = [
+  {id: 1, name: 'Amazon', location: 'Seattle'},
+  {id: 2, name: 'Apple', location: 'Cupertino'},
+  {id: 3, name: 'Facebook', location: 'Menlo Park'},
+  {id: 4, name: 'Google', location: 'Mountain View'},
+  {id: 5, name: 'Leeroy', location: 'Sundsvall'},
+  {id: 6, name: 'Tesla', location: 'Palo Alto'}
+]
 
-  const companies = [
-    {id: 1, name: 'Amazon', location: 'Seattle'},
-    {id: 2, name: 'Apple', location: 'Cupertino'},
-    {id: 3, name: 'Facebook', location: 'Menlo Park'},
-    {id: 4, name: 'Google', location: 'Mountain View'},
-    {id: 5, name: 'Leeroy', location: 'Sundsvall'},
-    {id: 6, name: 'Tesla', location: 'Palo Alto'}
-  ]
 
-})()
+let list = document.getElementById('list');
+
+for(var i = 0;i < companies.length; i++) {
+  list.innerHTML += "<li><div id='" + companies[i].name + "' class=\"oneLi\"><b>Name:</b> " + companies[i].name + " " + "<b>location:</b> " + companies[i].location + "</div></li>";
+};
+
+list.innerHTML += "<style>#list{cursor:pointer;}#list li{margin-bottom:10px;}</style>";
